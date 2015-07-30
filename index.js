1bbfbb9830bc2e55d1fe160d4c131fc24ec39e6f
Jordan Scales <scalesjordan@gmail.com>
2015-07-29 19:32:12 -0700

    added the appropriate fields to try the clabot-testing repo

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ var clabot = require("clabot");
 
 var app = clabot.createApp({
     getContractors: function(callback) {
-        callback(["list", "of", "contributors"]);
+        callback(["not-jdan"]);
     },
     token: process.env.GITHUB_TOKEN,
+    /*
     templates: {
         alreadySigned: "",
         notYetSigned: "",
         confirmSigned: "",
     },
+    */
     templateData: {
         link: "$http://your-cla-webform.com",
         maintainer: "$githubusername",
     },
     secrets: {
-        "$repoowner": {
-            "$sandbox": process.env.HUB_SECRET,
+        "khanbot": {
+            "clabot-testing": process.env.HUB_SECRET,
         },
     },
 });
